a67f6fc5384d8e558f78c6d6346c4b2f7c27c6e8
xseven56 <xseven56@gmail.com>
2023-03-04 12:04:21 +0500

    lesson#3

---
+++ homework_3.py
@@ -0,0 +1,45 @@
+
+
+
+# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+# list_1 = [1, 3, 6, 8, 4, 9]
+# for i in range(len(list_1)):
+#     break
+# print(*list_1)
+# from random import randint
+#
+# # lenght_array = int(input("Введите длину массива: "))
+# # number = int(input("Введите число, которое надо найти от 1 до 100: "))
+# #
+# # my_list = []
+# # count = 0
+# # for i in range(lenght_array):
+# #     current_mum = randint(1, 100)
+# #     my_list.append(current_mum)
+# #     if number == current_mum:
+# #         count = count + 1
+# # print(f'Число {number} встретилось в массиве {count} раз')
+# # print(my_list)
+#
+#
+# # задача 18
+#
+# lenght_array = int(input("Введите длину массива: "))
+# number = int(input("Введите искомое число от 1 до 100: "))
+#
+# my_list = []
+# close_number = 0
+# for i in range(lenght_array):
+#     current_mum = randint(1, 100)
+#     my_list.append(current_mum)
+#     if current_mum < number and close_number < current_mum:
+#         close_number = current_mum
+# print(f'К числу {number} ближайшее слева число {close_number}')
+# print(my_list)
+
+
+
+
+
